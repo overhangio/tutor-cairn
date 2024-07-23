76fcf04cbbd2ae295e7a990d0c47e9f246583445
Régis Behmo <regis@behmo.com>
2024-07-23 10:37:35 +0200

    build: fix readme content type warning

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
     maintainer_email="fahad.khalid@arbisoft.com",
     description="Scalable, real-time analytics for Open edX",
     long_description=load_readme(),
+    long_description_content_type="text/x-rst",
     packages=find_packages(exclude=["tests*"]),
     include_package_data=True,
     python_requires=">=3.8",
